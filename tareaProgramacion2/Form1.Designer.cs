33715448fa28900523ca6f763fe2ba00bdb00f5d
Paloma <palososa_19@hotmail.es>
2022-04-24 21:42:39 -0300

    first commit

---
+++ tareaProgramacion2/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿
+namespace tareaProgramacion2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblIntroducir = new System.Windows.Forms.Label();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.nmDía = new System.Windows.Forms.NumericUpDown();
+            this.nmMes = new System.Windows.Forms.NumericUpDown();
+            this.nmAño = new System.Windows.Forms.NumericUpDown();
+            this.lblSep1 = new System.Windows.Forms.Label();
+            this.lblSep2 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nmDía)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nmMes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nmAño)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblIntroducir
+            // 
+            this.lblIntroducir.AutoSize = true;
+            this.lblIntroducir.Font = new System.Drawing.Font("Berlin Sans FB Demi", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIntroducir.Location = new System.Drawing.Point(262, 144);
+            this.lblIntroducir.Name = "lblIntroducir";
+            this.lblIntroducir.Size = new System.Drawing.Size(170, 24);
+            this.lblIntroducir.TabIndex = 0;
+            this.lblIntroducir.Text = "Ingrese la fecha: ";
+            this.lblIntroducir.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Font = new System.Drawing.Font("Berlin Sans FB Demi", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFecha.Location = new System.Drawing.Point(262, 200);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(0, 24);
+            this.lblFecha.TabIndex = 1;
+            this.lblFecha.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblFecha.Visible = false;
+            // 
+            // nmDía
+            // 
+            this.nmDía.Location = new System.Drawing.Point(430, 148);
+            this.nmDía.Maximum = new decimal(new int[] {
+            31,
+            0,
+            0,
+            0});
+            this.nmDía.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nmDía.Name = "nmDía";
+            this.nmDía.Size = new System.Drawing.Size(44, 20);
+            this.nmDía.TabIndex = 2;
+            this.nmDía.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.nmDía.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // nmMes
+            // 
+            this.nmMes.Location = new System.Drawing.Point(497, 148);
+            this.nmMes.Maximum = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            this.nmMes.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nmMes.Name = "nmMes";
+            this.nmMes.Size = new System.Drawing.Size(49, 20);
+            this.nmMes.TabIndex = 3;
+            this.nmMes.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.nmMes.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // nmAño
+            // 
+            this.nmAño.Location = new System.Drawing.Point(572, 148);
+            this.nmAño.Maximum = new decimal(new int[] {
+            2022,
+            0,
+            0,
+            0});
+            this.nmAño.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.nmAño.Name = "nmAño";
+            this.nmAño.Size = new System.Drawing.Size(46, 20);
+            this.nmAño.TabIndex = 4;
+            this.nmAño.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.nmAño.Value = new decimal(new int[] {
+            2022,
+            0,
+            0,
+            0});
+            // 
+            // lblSep1
+            // 
+            this.lblSep1.AutoSize = true;
+            this.lblSep1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSep1.Location = new System.Drawing.Point(480, 148);
+            this.lblSep1.Name = "lblSep1";
+            this.lblSep1.Size = new System.Drawing.Size(14, 20);
+            this.lblSep1.TabIndex = 5;
+            this.lblSep1.Text = "/";
+            // 
+            // lblSep2
+            // 
+            this.lblSep2.AutoSize = true;
+            this.lblSep2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSep2.Location = new System.Drawing.Point(552, 148);
+            this.lblSep2.Name = "lblSep2";
+            this.lblSep2.Size = new System.Drawing.Size(14, 20);
+            this.lblSep2.TabIndex = 6;
+            this.lblSep2.Text = "/";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(624, 145);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 7;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lblSep2);
+            this.Controls.Add(this.lblSep1);
+            this.Controls.Add(this.nmAño);
+            this.Controls.Add(this.nmMes);
+            this.Controls.Add(this.nmDía);
+            this.Controls.Add(this.lblFecha);
+            this.Controls.Add(this.lblIntroducir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.nmDía)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nmMes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nmAño)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblIntroducir;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.NumericUpDown nmDía;
+        private System.Windows.Forms.NumericUpDown nmMes;
+        private System.Windows.Forms.NumericUpDown nmAño;
+        private System.Windows.Forms.Label lblSep1;
+        private System.Windows.Forms.Label lblSep2;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ tareaProgramacion2/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tareaProgramacion2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int dia = System.Convert.ToInt32(nmDía.Value);
+            string mes = System.Convert.ToString(nmMes.Value);
+            int año = System.Convert.ToInt32(nmAño.Value);
+
+            int diasMes = 0;
+            int esValido = 1;
+
+
+            if (mes == "1"|| mes == "3"|| mes == "5"|| mes == "7"|| mes == "8"|| mes == "10"|| mes == "12")
+            {
+                diasMes = 1;
+            }
+            else if (mes == "2")
+            {
+                diasMes = 3;
+            }
+            else
+            {
+                diasMes = 2;
+            }
+
+            switch (diasMes)
+            {
+                case 1:
+                    if (dia == 31)
+                    {
+               
+                        dia = 1;
+                        if (mes == "12")
+                        {
+                            año = año + 1;
+                            mes = "1";
+                        }
+                        else
+                        {
+                            int numeroMes = System.Convert.ToInt32(mes);
+                            numeroMes = numeroMes + 1;
+                            mes = System.Convert.ToString(numeroMes);
+                        }
+                        
+                    }
+                    else if (dia > 31)
+                    {
+                        esValido = 0;
+                    }
+                    else
+                    {
+                        dia = dia + 1;
+                    };
+                    break;
+                case 2:
+                    if (dia == 30)
+                    {
+                        dia = 1;
+                        int numeroMes = System.Convert.ToInt32(mes);
+                        numeroMes = numeroMes + 1;
+                        mes = System.Convert.ToString(numeroMes);
+                    }
+                    else if (dia > 30)
+                    {
+                        esValido = 0;
+                    }
+                    else
+                    {
+                        dia = dia + 1;
+                    };
+                    break;
+                case 3:
+                    if (dia == 28)
+                    {
+                        dia = 1;
+                        int numeroMes = System.Convert.ToInt32(mes);
+                        numeroMes = numeroMes + 1;
+                        mes = System.Convert.ToString(numeroMes);
+                    }
+                    else if (dia > 28)
+                    {
+                        esValido = 0;
+                    }
+                    else
+                    {
+                        dia = dia + 1;
+                    };
+                    break;
+                default:
+                    break;
+            }
+
+            switch (mes)
+            {
+                case "1":
+                    mes = "Enero";
+                    break;
+                case "2":
+                    mes = "Febrero";
+                    break;
+                case "3":
+                    mes = "Marzo";
+                    break;
+                case "4":
+                    mes = "Abril";
+                    break;
+                case "5":
+                    mes = "Mayo";
+                    break;
+                case "6":
+                    mes = "Junio";
+                    break;
+                case "7":
+                    mes = "Julio";
+                    break;
+                case "8":
+                    mes = "Agosto";
+                    break;
+                case "9":
+                    mes = "Septiembre";
+                    break;
+                case "10":
+                    mes = "Octubre";
+                    break;
+                case "11":
+                    mes = "Noviembre";
+                    break;
+                case "12":
+                    mes = "Diciembre";
+                    break;
+                default:
+                    break;
+            }
+
+
+            if (esValido == 1)
+            {
+                lblFecha.Text = "El día siguiente es " + dia + " de " + mes + " de " + año;
+                lblFecha.Visible = true;
+            }
+            else
+            {
+                lblFecha.Text = "Fecha no válida";
+                lblFecha.Visible = true;
+            }
+        }
+
+        
+}
+}
